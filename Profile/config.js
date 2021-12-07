30cd5947cc63bc6ff428400f563092c3c3018f43
DauPhu2005 <95694471+DauPhu2005@users.noreply.github.com>
2021-12-07 20:13:00 +0700

    Add files via upload

---
+++ Profile/config.js
@@ -0,0 +1,118 @@
+/*Vui Lòng không xoá dòng này để tôn trọng người làm ra web
+Wbsite được thiết kế bởi Nguyễn-Quốc-Vinh. 
+//  _   _                                  ____                        _   _ _       _   
+// | \ | |                                /    \                      | | | (_)     | | 
+// |  \| | __   _   _ _   _  ___ _ __    '  __  '         ___   ___   | | | |_ _ __ | |___
+// | . ` |/ _` | | | | | | |/ _ \ '_ \   | |__| | | | | |/ _ \ /  _\  | | | | | '_ \|  _  \
+// | |\  | (_| | |_| | |_| |  __/ | | |  .    \ \ | |_| | (_) |  (_   \ \_/ / | | | | | | |
+// \_| \_/\__, |\__,_|\__, |\___|_| |_|   \___/\_\ \__,_|\___/ \___/   \___/|_|_| |_|_| |_/
+//         __/ |       __/ |  
+//        |___/       |___/ 
+Nếu bạn xoá cái này thì bạn không phải là con người*/
+const WEB = {
+    title: "Buy me a coffee !",
+    favicon: "https://imgur.com/H6UzxNg.png",
+};
+
+const USER = {
+    username: "Chị Em Cây Khế - Tổ 4",
+    blueTick: true,
+    skills: [
+        "Group",
+        "Profile",
+    ],
+    banks: [
+        {
+            logo: "https://imgur.com/xz5HY3v.png",
+            vinhdzvcl: "Vũ Duy Mạnh",
+            ChucVu: "Tổ trưởng",
+            NgaySinh: "07/12/2005",
+            FB: "Đậu Phụ Nè"
+        },
+        {
+            logo: "https://imgur.com/8KaApk7.png",
+            vinhdzvcl: "Lường Ngọc Tú",
+            ChucVu: "Thành Viên",
+            NgaySinh: "11/05/2005",
+            FB: "Luong Tú"
+        },
+        {
+            logo: "https://imgur.com/JV80ocw.png",
+            vinhdzvcl: "Ngô Bá Vũ",
+            ChucVu: "Thành Viên",
+            NgaySinh: "19/05/2005",
+            FB: "Ngô Vũ"
+        },
+        {
+            logo: "https://imgur.com/xqMUZNo.png",
+            vinhdzvcl: "Nguyễn Văn Đại",
+            ChucVu: "Thành Viên",
+            NgaySinh: "14/05/2005",
+            FB: "Đại Đây"
+        },
+        {
+            logo: "https://imgur.com/TomPkIP.png",
+            vinhdzvcl: "Phạm Tiến Quang",
+            ChucVu: "Thành Viên",
+            NgaySinh: "24/11/2005",
+            FB: "Phạm Tiến Quang"
+        },
+        {
+            logo: "https://imgur.com/q7PVLnM.png",
+            vinhdzvcl: "Triệu Hồng Ngọc",
+            ChucVu: "Thành Viên",
+            NgaySinh: "03/01/2005",
+            FB: "Hồngg Ngọc"
+        },
+        {
+            logo: "https://imgur.com/oYJvQTA.png",
+            vinhdzvcl: "Đinh Hà Hương",
+            ChucVu: "Thành Viên",
+            NgaySinh: "14/10/2005",
+            FB: "Đinh H. Hương"
+        },
+        {
+            logo: "https://imgur.com/70tmZQ5.png",
+            vinhdzvcl: "Hoàng Thu Thảo",
+            ChucVu: "Thành Viên",
+            NgaySinh: "08/12/2005",
+            FB: "Hoàng Thu Thảo"
+        },
+        {
+            logo: "",
+            vinhdzvcl: "Lục Thị Mai",
+            ChucVu: "Thành Viên",
+            NgaySinh: "06/05/2005",
+            FB: "Lục Mai"
+        },
+        {
+            logo: "https://imgur.com/Gu0qNnO.png",
+            vinhdzvcl: "Triệu Thanh Tâm",
+            ChucVu: "Thành Viên",
+            NgaySinh: "13/3/2005",
+            FB: "Tr Tâm"
+        },
+        {
+            logo: "https://imgur.com/iam6zYX.png",
+            vinhdzvcl: "Triệu Thị Kim Thoa",
+            ChucVu: "Cựu Thành Viên",
+            NgaySinh: "11/11/2005",
+            FB: "Kim Thoaa"
+        },
+        {
+            logo: "https://imgur.com/6tKJNFz.png",
+            vinhdzvcl: "Hoàng Mai Ngọc",
+            ChucVu: "Cựu Thành Viên",
+            NgaySinh: "26/1/2005",
+            FB: "Hoàngg Maii Ngọcc"
+        },
+        {
+            logo: "https://imgur.com/1MdIPQK.png",
+            vinhdzvcl: "Nguyễn Quỳnh Trang",
+            ChucVu: "Cựu Thành Viên",
+            NgaySinh: "27/06/2005",
+            FB: "Quỳnh Trang"
+        },
+        
+    ]
+}
